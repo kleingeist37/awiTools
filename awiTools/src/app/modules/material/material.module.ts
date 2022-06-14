2daa61030b86fbe5a2f70c108a945ed18a73c75a
kleingeist <kleingeist@awiworks.com>
2022-06-14 19:44:12 +0200

    added exports to material module

--- awiTools/src/app/modules/material/material.module.ts
+++ awiTools/src/app/modules/material/material.module.ts
@@ -1,12 +1,49 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 
+
+import {MatSelectModule} from '@angular/material/select';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatTreeModule} from '@angular/material/tree';
 
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
+  exports: [
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+
+
+
+    MatSelectModule,
+    MatSnackBarModule,
+
+    
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule
+    
   ]
 })
 export class MaterialModule { }
