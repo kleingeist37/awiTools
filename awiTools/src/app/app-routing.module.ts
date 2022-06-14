84bc89ed686983ccac9b624d1d93c7ed4e4160c9
kleingeist <kleingeist@awiworks.com>
2022-06-14 20:16:19 +0200

    added navigation service, ideas board module, created initial routes

--- awiTools/src/app/app-routing.module.ts
+++ awiTools/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'ideas', loadChildren: () => import('./modules/ideas-board/ideas-board.module').then(m => m.IdeasBoardModule)},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+     scrollPositionRestoration: 'enabled'
+    ,preloadingStrategy: PreloadAllModules
+  })
+],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- awiTools/src/app/shared/services/navigation.service.ts
+++ awiTools/src/app/shared/services/navigation.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { NavElement } from '../interfaces/nav-element.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavigationService {
-
+  private navElements : NavElement[] = [
+    {id: 'nav-home', name: 'Home', route: '', icon: 'home'},
+    {id: 'nav-idea', name: 'Ideas Board', route: '/ideas', icon: 'lightbulb'},
+  ]
+  
+  
   constructor() { }
 }
